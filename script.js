f0cb08a5b8a8210f273a7b43579dab65b0649514
Yatoorooo <Egvarakcin@gmail.com>
2023-12-27 17:13:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let myNumber1;
+let myNumber2;
+
+myNumber1 = +prompt('Write your nubmer one:' );
+myNumber2 = +prompt('Write your number two:' );
+
+alert(`${myNumber1} + ${myNumber2} = ${myNumber1 + myNumber2}
+${myNumber1} - ${myNumber2} = ${myNumber1 - myNumber2}
+${myNumber1} * ${myNumber2} = ${myNumber1 * myNumber2}
+${myNumber1} / ${myNumber2} = ${myNumber1 / myNumber2}
+`);                  
